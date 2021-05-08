91c56aef72934c6db24b10fbdb52afd748078f2b
Richard <rtcloud2@gmail.com>
2021-05-08 11:52:53 -0400

    added custom exceptions

---
+++ src/main/java/com/revature/Exceptions/InvalidRequestException.java
@@ -0,0 +1,10 @@
+package com.revature.Exceptions;
+
+public class InvalidRequestException extends Exception {
+
+    public  InvalidRequestException (String message) {
+        super(message);
+    }
+
+}
+
---
+++ src/main/java/com/revature/Exceptions/ResourcePersistenceException.java
@@ -0,0 +1,9 @@
+package com.revature.Exceptions;
+
+public class ResourcePersistenceException extends Exception {
+
+    public ResourcePersistenceException(String message) {
+        super(message);
+    }
+
+}
