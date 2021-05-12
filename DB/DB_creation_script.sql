96a977081610e28df73fee054c19096f05f185f1
Richard <rtcloud2@gmail.com>
2021-05-12 19:14:27 -0400

    Added database script and ERD

---
+++ DB/DB_creation_script.sql
@@ -0,0 +1,62 @@
+create table users (
+	userid serial,
+	username varchar(50) unique not null,
+	"password" text not null,
+	email varchar(255) unique not null,
+	first_name varchar(50) not null,
+	last_name varchar(50) not null,
+	birthday timestamp not null,
+	joined_date timestamp not null,
+	"age" int check(age > 0) not null,
+	constraint pk_users primary key (userid)
+);
+
+create table account (
+	accountid serial primary key,
+	account_name varchar(20) not null,
+	balance decimal(10,2)
+);
+
+create table users_account (
+	accountid int not null,
+	userid int not null,
+	
+	constraint pk_users_account
+	primary key(accountid, userid),
+	
+	constraint fk_user
+	foreign key(userid)
+	references users(userid)
+	on delete cascade,
+	
+	constraint fk_account
+	foreign key(accountid)
+	references account(accountid)
+	on delete cascade
+);
+
+create table transactions (
+	transactionid serial,
+	sender_name varchar(50) not null,
+	sender_account int not null,
+	recipient_name varchar(50) not null,
+	recipient_account int not null,
+	transaction_type varchar(10),
+	amount decimal(10,2) not null,
+	transaction_date timestamp,
+	constraint pk_transaction primary key(transactionid),
+	constraint fk_account 
+	foreign key(sender_account) 
+	references account (accountid),
+	
+	constraint fk_account2
+	foreign key(recipient_account)
+	references account(accountid)
+	on delete cascade,
+	
+	constraint fk_account --scuffed, I know
+	foreign key(sender_account)
+	references account(accountid)
+	on delete cascade;
+
+);
